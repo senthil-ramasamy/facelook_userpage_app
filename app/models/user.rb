e37836c030cc8593209f250022f44191829ba81c
senthil-ramasamy <www.senthilr.com@gmail.com>
2014-06-14 16:09:18 +0530

    image bug fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/default.png",
-  :url => "/assets/users/:id/:style/:basename.:extension",
-  :path => ":rails_root/users/assets/products/:id/:style/:basename.:extension"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/default.png"
+
+#  ,:url => "/assets/users/:id/:style/:basename.:extension",
+#  :path => ":rails_root/users/assets/products/:id/:style/:basename.:extension"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
